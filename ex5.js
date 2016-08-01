7d9cb9f5e5700298a9c20bad6818824d29db8e15
Nuria <soriano.nuria@gmail.com>
2016-08-01 17:02:18 +0200

    extra ex5

--- ex5.js
+++ ex5.js
@@ -2,4 +2,10 @@ var fs = require('fs');
 var destFile = process.argv[2];
 var txt = process.argv[3];
 
-fs.writeFile(destFile, txt)
+fs.writeFile(destFile, txt, function(err){
+	if (err) throw err;
+	console.log('The file was saved!')
+	fs.readFile(destFile, 'utf8', function(err, data){
+		console.log(data)
+	})
+})
